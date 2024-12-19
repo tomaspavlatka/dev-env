825c4ef63c9e638d2e15279fe6692d29ad24eb4d
Tomas Pavlatka <tomas.pavlatka@zolar.de>
2024-12-19 08:56:43 +0100

    adding oil.nvim and also using pa for harpoon menu

--- env/.config/nvim/lua/tomaspavlatka/plugins/harpoon.lua
+++ env/.config/nvim/lua/tomaspavlatka/plugins/harpoon.lua
@@ -3,7 +3,7 @@ return {
     "ThePrimeagen/harpoon",
     keys = {
       { "<leader>a",  function() require("harpoon.mark").add_file() end,        mode = { "n" } },
-      { "<leader>gh", function() require("harpoon.ui").toggle_quick_menu() end, mode = { "n" } },
+      { "<leader>pa", function() require("harpoon.ui").toggle_quick_menu() end, mode = { "n" } },
       { "<C-j>",      function() require("harpoon.ui").nav_file(1) end,         mode = { "n" } },
       { "<C-k>",      function() require("harpoon.ui").nav_file(2) end,         mode = { "n" } },
       { "<C-l>",      function() require("harpoon.ui").nav_file(3) end,         mode = { "n" } },
