08454c880f6b340a25501a3044505d441dfe359d
Tomas Pavlatka <tomas@pavlatka.cz>
2025-11-12 12:27:38 +0100

    restart

--- README.md
+++
@@ -1,2 +0,0 @@
-# dev-env
-My development environment
--- env/.config/nvim/lua/tomaspavlatka/plugins/flash.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" }
-  },
-}
