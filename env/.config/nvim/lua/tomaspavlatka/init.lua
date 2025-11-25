c823f3dcd14a4283df1bc7d2587daa80c43aff98
Tomas Pavlatka <tomas@pavlatka.cz>
2025-11-25 17:25:33 +0100

    working version of the lsp

--- env/.config/nvim/lua/tomaspavlatka/init.lua
+++ env/.config/nvim/lua/tomaspavlatka/init.lua
@@ -1,6 +1,6 @@
 require("tomaspavlatka/remap")
 require("tomaspavlatka/set")
-
+require("tomaspavlatka/lsp")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
