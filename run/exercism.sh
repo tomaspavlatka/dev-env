cf1447fed7d1121cf5eb1eb6e241700a58bd18b0
Tomas Pavlatka <tomas.pavlatka@forto.com>
2026-01-02 11:16:11 +0100

    removing what's not needed

--- run/exercism.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if brew list --formula | grep -q "^exercism$"; then
-  echo "exercism is already installed. Attempting to update..."
-  brew upgrade exercism
-else
-  echo "exercism is not installed. Installing..."
-  brew install exercism
-fi
