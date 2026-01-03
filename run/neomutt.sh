165b43c00852a11bf0de7d887bc9805e1f9b880b
Tomas Pavlatka <tomas.pavlatka@forto.com>
2026-01-03 05:22:34 +0100

    removing neomutt

--- run/neomutt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if brew list --formula | grep -q "^neomutt$"; then
-  echo "neomutt is already installed. Attempting to update..."
-  brew upgrade neomutt
-else
-  echo "neomutt is not installed. Installing..."
-  brew install neomutt
-fi
