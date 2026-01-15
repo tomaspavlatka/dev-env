b4e188475b3bbb77481c3c349a2f850c42d87260
Tomas Pavlatka <tomas.pavlatka@forto.com>
2026-01-15 09:15:15 +0100

    moving to 4 spaces for TS as well

--- env/.config/nvim/lua/tomaspavlatka/set.lua
+++ env/.config/nvim/lua/tomaspavlatka/set.lua
@@ -38,13 +38,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
--- TypeScript: 2 spaces
+-- TypeScript: 4 spaces
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "typescript",
   callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 4
     vim.opt_local.expandtab = true
   end
 })
