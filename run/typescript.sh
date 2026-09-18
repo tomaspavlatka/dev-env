b13dd414a29a87127dbd85d66f400a5bce8c2a31
Tomas Moya Pavlatka <tomas@pavlatka.cz>
2026-09-18 09:51:02 +0200

    using typescript 6

--- run/typescript.sh
+++ run/typescript.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 # TypeScript compiler (tsc) plus its language server, used by the ts_ls LSP in Neovim.
-npm install -g typescript typescript-language-server
+#
+# typescript is pinned to the 6.x line: 7.x is the native Go rewrite and no longer
+# ships lib/tsserver.js, which typescript-language-server requires to start.
+npm install -g typescript@6 typescript-language-server
